e8b293c6813b5479434161054b9d48ef0d4a3da6
Alex Buznik <shu@buznik.net>
2019-05-11 20:38:26 +0300

    Basic ApiRequest test

---
+++ test/ApiRequest.test.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const chai = require('chai')
+const ApiRequest = require('../lib/ApiRequest')
+
+const CORRECT_CONFIG = {
+  API_URL: 'url',
+  API_DELAY: 60000
+}
+
+describe('ApiRequest instance', function() {
+  it('should instantiate with config', function() {
+    const apiRequest = new ApiRequest(CORRECT_CONFIG)
+  })
+
+  it('should fail without config', function() {
+    chai.assert.throws(function() {
+      const apiRequest = new ApiRequest()
+    }, 'Options is not an object')
+  })
+
+  it('should fail without API_URL option', function() {
+    chai.assert.throws(function() {
+      const apiRequest = new ApiRequest({
+        API_DELAY: 1
+      })
+    }, 'missing API_URL option')
+  })
+
+  it('should fail with empty API_URL option', function() {
+    chai.assert.throws(function() {
+      const apiRequest = new ApiRequest({
+        API_DELAY: 1,
+        API_URL: ''
+      })
+    }, 'API_URL is empty')
+  })
+
+  it('should fail without API_DELAY option', function() {
+    chai.assert.throws(function() {
+      const apiRequest = new ApiRequest({
+        API_URL: '1'
+      })
+    }, 'API_DELAY option should be a number')
+  })
+
+  it('should fail with API_DELAY less than 1', function() {
+    chai.assert.throws(function() {
+      const apiRequest = new ApiRequest({
+        API_URL: '1',
+        API_DELAY: 0
+      })
+    }, 'API_DELAY should be greater than 0')
+  })
+})
+
+
+
+
